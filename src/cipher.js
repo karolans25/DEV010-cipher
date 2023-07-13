7f3820df27f440cb24b5653e3652a4455e79e34c
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-07-13 04:21:10 -0500

    Implementadas las funcionalidades de decode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,57 @@
+// MAYÚSCULAS, Ñ = 165 
+// minúsculas, ñ = 164
+// numeros
+//const min = 48, max = 57; // en ASCII
+const min_M = 65, max_M = 90, min_m = 97, max_m = 122; // en ASCII
+
+const alphabet_M = [], alphabet_m = [];
+
+for(let i = min_M; i<=max_M; i++){
+  alphabet_M.push(String.fromCharCode(i));
+}
+
+for(let i = min_m; i<=max_m; i++){
+  alphabet_m.push(String.fromCharCode(i));
+}
+
 const cipher = {
-  // ...
-};
+  encode: function(offset, txt){
+    let result = "", mod = 0;
+    for (let i = 0; i < txt.length; i++){
+      if (txt[i] === txt[i].toUpperCase()){
+        mod = (alphabet_M.indexOf(txt[i])+parseInt(offset))%alphabet_M.length;
+        result += alphabet_M[mod];
+      } else if (txt[i] === txt[i].toLowerCase()){
+        mod = (alphabet_m.indexOf(txt[i])+parseInt(offset))%alphabet_m.length;
+        result += alphabet_m[mod];
+      }
+    }
+    //console.log(result);
+    return result;
+  },
+
+  decode: function(offset, txt){
+    let result = "", mod = 0;
+    for (let i = 0; i < txt.length; i++){
+      if (txt[i] === txt[i].toUpperCase()){
+        mod = alphabet_M.indexOf(txt[i])-parseInt(offset);
+        if (mod < 0 && mod !== -26){
+          mod = alphabet_M.length + mod%alphabet_M.length;
+        } else {
+          mod = 0;
+        }
+        result += alphabet_M[mod];
+      } else if (txt[i] === txt[i].toLowerCase()){
+        mod = alphabet_m.indexOf(txt[i])-parseInt(offset);
+        if (mod < 0){
+          mod = alphabet_m.length + mod;
+        }
+        result += alphabet_M[mod];
+      }
+    }
+    //console.log(result);
+    return result;
+  }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//CASO 1: Codificar
+//let offset = 13, txt = "LAROSADEGUADALUPE";
+//console.log(cipher.encode(offset, txt));
+
+const offset = 33; 
+let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+alert(cipher.encode(offset, txt));
+txt = "HIJKLMNOPQRSTUVWXYZABCDEFG";
+alert(cipher.decode(offset, txt));
+
+//CASO 2: Decodificar
+//let offset = 13, txt = "YNEBFNQRTHNQNYHCR";
+//console.log(cipher.decode(13, "YNEBFNQRTHNQNYHCR"));
+
+//CASO 3: Codificar y decodificar con distinto offset
+//let offset = 5, txt = cipher.encode(offset, "LAROSADEGUADALUPE"));
+//offset = 13;
+//console.log(cipher.decode(offset, txt));
